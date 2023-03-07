eeeedaa62ba2fd37300c11514f1f0a9a5480426d
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-03-07 17:36:37 +0500

    Login

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 import express from "express";
 import cors  from "cors";
+// import Auth from "./routes/auth.js";
 
 const app = express();
 
 app.use(cors());
+// app.use(Auth)
 app.use(express.json());
+
+
+app.post('/login', (req, res) => {
+    res.redirect('/');
+    console.log(req.body)
+});
 
 app.get('/', (req, res) => {
     res.render('../client/src/App')
 });
+
 
 
 app.listen(3000, () => {
